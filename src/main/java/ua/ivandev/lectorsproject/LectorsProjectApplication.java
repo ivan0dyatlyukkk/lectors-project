c8e432648aa595dea12aa9dda9c45f8a584045da
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-11-04 11:31:00 +0200

    inital project

---
+++ src/main/java/ua/ivandev/lectorsproject/LectorsProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.ivandev.lectorsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectorsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectorsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/ivandev/lectorsproject/LectorsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ivandev.lectorsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectorsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
